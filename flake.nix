f4cc0272cda509ee44e36e6e1b23add7e3cc279e
Firman Lestari <jusles363@gmail.com>
2026-03-08 13:19:35 +0700

    DU-36: update depedencies vuln

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Pixtern development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    {
+      devShells = nixpkgs.lib.genAttrs
+        [
+          "aarch64-darwin"
+          "x86_64-darwin"
+          "x86_64-linux"
+          "aarch64-linux"
+        ]
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          {
+            default = pkgs.mkShell {
+              name = "pixtern-shell";
+
+              packages = with pkgs; [
+                nodejs_22
+                bun
+              ];
+
+              shellHook = ''
+                echo "🚀 Node: $(node -v)"
+                echo "⚡ Bun: $(bun --version)"
+              '';
+            };
+          }
+        );
+    };
+}
